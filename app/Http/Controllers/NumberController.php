ecff7d768071cbbb7c6d6b74bfb33fb2a613f5c8
DESKTOP-GODWIN\godwin <gtw2407@gmail.com>
2018-08-18 13:34:31 +0200

    Finding primes consecutives

--- app/Http/Controllers/NumberController.php
+++ app/Http/Controllers/NumberController.php
@@ -11,34 +11,20 @@ class NumberController extends Controller
     public function index(Request $request)
     {
         set_time_limit(100);
+        $timeStart = microtime(true);
+        $number = 1000; //(int) $request->get('number');
         
-        $number = 10000; //(int) $request->get('number');
-        
-        if ($number > 1) {
-            
-            for ($i = 1; $i <= $number; $i++) {
-                
-                $countAnswers = 0;
-                
-                for ($j = 1; $j <= $i; $j++) {
-                    
-                    if ($i % $j === 0) {
-                        $countAnswers++;
-                        if ($countAnswers > 2) {
-//                            echo $i . ' not a prime<br/>';
-                            break;
-                        }
-                    }
-                }
-            
-                if ($countAnswers == 2) {
-//                    echo $i . ' is a prime number<br/>';
-                    $this->primes[] = $i;
-                }
+        $prime = new \App\Models\Number($number);
+        $primes = $prime->getPrimes();
+
+        foreach ($primes as $primeNumber) {
+            $consecutives = $prime->getConsecutivePrimes($primeNumber);
+            if ($consecutives) {
+                echo "<pre>" . print_r($consecutives, 1) . "</pre>";
+                echo __METHOD__ . ': ' . __LINE__ . "</br>";
             }
         }
         
-        echo "<pre>" . print_r($this->primes, 1) . "</pre>";
-        echo __METHOD__ . ': ' . __LINE__ . "</br>";
+//        return view('number.index', $data);
     }
 }
--- app/Models/Number.php
+++ app/Models/Number.php
@@ -6,5 +6,86 @@ use Illuminate\Database\Eloquent\Model;
 
 class Number extends Model
 {
-    //
+    private $primes;
+    
+    private $number;
+    
+    public function __construct($number, array $attributes = array())
+    {
+        parent::__construct($attributes);
+        
+        $this->number = $number > 1 ? (int) $number : 1;
+        $this->primes = [];
+    }
+    
+    /**
+     * Finding prime should meet two conditions
+     * - It is divisible by 1 
+     * - It is divisible only by itself
+     */
+    public function getPrimes()
+    {
+        if ($this->number > 1) {
+            
+            // Looping through every number 
+            for ($i = 1; $i <= $this->number; $i++) {
+                
+                $countAnswers = 0;
+                
+                // Loop through numbers till current number
+                for ($j = 1; $j <= $i; $j++) {
+                    
+                    // Divisible number
+                    if ($i % $j === 0) {
+                        $countAnswers++;
+                        // number is divisible more than two number, not a prime
+                        if ($countAnswers > 2) {
+                            break;
+                        }
+                    }
+                }
+            
+                // Prime number found, conditions met
+                if ($countAnswers == 2) {
+                    $this->primes[] = $i;
+                }
+            }
+        }
+        
+        return $this->primes;
+    }
+    
+    public function getConsecutivePrimes($answerNumber)
+    {
+        $answerNumber = (int) $answerNumber;
+        if ($answerNumber > $this->number) {
+            throw new Exception('answer must be lower than number');
+        }
+        if (empty($this->primes)) {
+            $this->getPrimes();
+        }
+        
+        $addUp = 0;
+        $addUpText = [];
+        
+        // For every prime number to add up for the answer
+        foreach ($this->primes as $prime) {
+            $addUp += $prime;
+            $addUpText[] = $prime;
+            
+            // The consecutive numbers add up which have an exact number to answer
+            // Also stop adding numbers if the add ups is greater than the answer
+            if ($addUp === $answerNumber) {
+                
+                $addUpText = $answerNumber . ' = ' . implode(' + ', $addUpText);
+                return $addUpText;
+                
+            } elseif ($addUp > $answerNumber) {
+                $addUpText = $answerNumber . ' = ' . implode(' + ', $addUpText);
+                echo "<pre>" . print_r($addUpText, 1) . "</pre>";
+                echo __METHOD__ . ': ' . __LINE__ . "</br>";
+                break;
+            }
+        }
+    }
 }
